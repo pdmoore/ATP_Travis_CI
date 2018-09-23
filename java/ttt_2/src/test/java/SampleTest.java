3961f1d0cc16d36220a433d6b0a7b5c700544904
Paul Moore <pdmoore@gmail.com>
2018-09-23 11:19:02 -0700

    add a second java project to the build

---
+++ java/ttt_2/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void exampleTest() {
+        Assert.assertTrue("Example of a passing assertion", true);
+//        Assert.fail("break travis");
+    }
+}
